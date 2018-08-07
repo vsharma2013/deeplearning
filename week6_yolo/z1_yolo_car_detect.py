cc63f92cb4f50a2ed4d3a90982ae14bc117f7714
vishal <vishal2033@gmail.com>
2018-08-07 11:14:05 +0530

    yolo init

--- week6_yolo/z1_yolo_car_detect.py
+++ week6_yolo/z1_yolo_car_detect.py
@@ -15,5 +15,54 @@ from keras.models import load_model, Model
 from week6_yolo.yolo_utils import read_classes, read_anchors, generate_colors, preprocess_image, draw_boxes, scale_boxes
 from week6_yolo.keras_yolo import yolo_head, yolo_boxes_to_corners, preprocess_true_boxes, yolo_loss, yolo_body
 
+
+def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=.6):
+    """Filters YOLO boxes by thresholding on object and class confidence.
+
+    Arguments:
+    box_confidence -- tensor of shape (19, 19, 5, 1)
+    boxes -- tensor of shape (19, 19, 5, 4)
+    box_class_probs -- tensor of shape (19, 19, 5, 80)
+    threshold -- real value, if [ highest class probability score < threshold], then get rid of the corresponding box
+
+    Returns:
+    scores -- tensor of shape (None,), containing the class probability score for selected boxes
+    boxes -- tensor of shape (None, 4), containing (b_x, b_y, b_h, b_w) coordinates of selected boxes
+    classes -- tensor of shape (None,), containing the index of the class detected by the selected boxes
+
+    Note: "None" is here because you don't know the exact number of selected boxes, as it depends on the threshold.
+    For example, the actual output size of scores would be (10,) if there are 10 boxes.
+    """
+
+    # Step 1: Compute box scores
+    # ## START CODE HERE ### (≈ 1 line)
+    box_scores = box_confidence * box_class_probs
+
+    # ## END CODE HERE ###
+
+    # Step 2: Find the box_classes thanks to the max box_scores, keep track of the corresponding score
+    # ## START CODE HERE ### (≈ 2 lines)
+    box_classes = K.argmax(box_scores, -1)
+    box_classes_scores = K.max(box_scores, -1)
+
+    filter_mask = box_classes_scores > threshold
+
+    scores = tf.boolean_mask(box_classes_scores, filter_mask)
+    classes = tf.boolean_mask(box_classes, filter_mask)
+    boxes = tf.boolean_mask(boxes, filter_mask)
+
+    return scores, boxes, classes
+
 def run():
-    print("\n\n\n I am YOLO ")
+    with tf.Session() as test_a:
+        box_confidence = tf.random_normal([19, 19, 5, 1], mean=1, stddev=4, seed=1)
+        boxes = tf.random_normal([19, 19, 5, 4], mean=1, stddev=4, seed=1)
+        box_class_probs = tf.random_normal([19, 19, 5, 80], mean=1, stddev=4, seed=1)
+        scores, boxes, classes = yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=0.5)
+        print("scores[2] = " + str(scores[2].eval()))
+        print("boxes[2] = " + str(boxes[2].eval()))
+        print("classes[2] = " + str(classes[2].eval()))
+        print("scores.shape = " + str(scores.shape))
+        print("boxes.shape = " + str(boxes.shape))
+        print("classes.shape = " + str(classes.shape))
+
